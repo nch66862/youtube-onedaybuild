511af22f98a398a1764517aa5d59ccf9a948d896
Nicholas Carver <nickcarver@Nicholass-MacBook-Pro.local>
2020-06-18 21:48:26 -0500

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Nicholas Carver on 6/18/20.
+//  Copyright © 2020 Nicholas Carver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
